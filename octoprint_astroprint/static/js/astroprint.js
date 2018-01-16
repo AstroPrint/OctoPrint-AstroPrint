ab7e0dd159664d7b5d2ab427695d17d39649587e
alfonsopl <alfonsoplgu@gmail.com>
2018-01-16 10:04:08 +0100

    Uri encoded before send

--- octoprint_astroprint/static/js/astroprint.js
+++ octoprint_astroprint/static/js/astroprint.js
@@ -351,7 +351,9 @@ $(function () {
         self.authorizeAstroprint = function () {
             var ap_access_key = self.access_key()
             if (ap_access_key){
-              var currentUrl = encodeURI(window.location.href.split('?')[0]);
+              var currentUrl = window.location.href.split('?')[0];
+              currentUrl = window.location.href.split('#')[0];
+              currentUrl = encodeURI(currentUrl);
               var url = astroprint_variables.appSite + "/authorize" +
                   "?client_id=" + astroprint_variables.appId +
                   "&redirect_uri=" + currentUrl +
