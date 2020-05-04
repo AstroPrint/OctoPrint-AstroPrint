276dd7a867344098dd18d966dfc53ccaf7e8b9ef
Daniel Arroyo <daniel.arroyo@gmail.com>
2020-05-04 13:10:10 +0200

    use full url encoded

--- octoprint_astroprint/static/js/astroprint.js
+++ octoprint_astroprint/static/js/astroprint.js
@@ -706,11 +706,9 @@ $(function () {
         self.authorizeAstroprint = function () {
             var ap_access_key = self.access_key()
             if (ap_access_key){
-              var currentUrl = window.location.href.split('?')[0];
-              currentUrl = window.location.href.split('#')[0];
               var url = astroprint_variables.appSite + "/authorize?" +
                   "client_id=" + astroprint_variables.appId +
-                  "&redirect_uri=" + encodeURIComponent(currentUrl) +
+                  "&redirect_uri=" + encodeURIComponent(window.location.href) +
                   "&scope=" + encodeURIComponent("profile:read project:read design:read design:download print-file:read print-file:download print-job:read device:connect device:update device:read") +
                   "&state="+ap_access_key+
                   "&response_type=code";
